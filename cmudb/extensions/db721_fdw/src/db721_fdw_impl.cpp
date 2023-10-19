1a1a2f4e7f0fc46756f9467114a9e51f8c48c6f8
Yoshiyuki Komazaki <komukomo@users.noreply.github.com>
2023-10-19 14:33:24 +0000

    return empty table

--- cmudb/extensions/db721_fdw/src/db721_fdw_impl.cpp
+++ cmudb/extensions/db721_fdw/src/db721_fdw_impl.cpp
@@ -8,6 +8,9 @@ extern "C" {
 #include "../../../../src/include/postgres.h"
 #include "../../../../src/include/fmgr.h"
 #include "../../../../src/include/foreign/fdwapi.h"
+#include "../../../../src/include/optimizer/pathnode.h"
+#include "../../../../src/include/optimizer/restrictinfo.h"
+#include "../../../../src/include/optimizer/planmain.h"
 }
 // clang-format on
 
@@ -23,6 +26,18 @@ extern "C" void db721_GetForeignPaths(PlannerInfo *root, RelOptInfo *baserel,
   // TODO(721): Write me!
   Dog scout("Scout");
   elog(LOG, "db721_GetForeignPaths: %s", scout.Bark().c_str());
+
+  Path *path = (Path *)create_foreignscan_path(root, baserel,
+    NULL,
+    baserel->rows,
+    1,
+    1 + baserel->rows,
+    NIL,
+    NULL,
+    NULL,
+    NIL
+  );
+  add_path(baserel, path);
 }
 
 extern "C" ForeignScan *
@@ -30,7 +45,17 @@ db721_GetForeignPlan(PlannerInfo *root, RelOptInfo *baserel, Oid foreigntableid,
                    ForeignPath *best_path, List *tlist, List *scan_clauses,
                    Plan *outer_plan) {
   // TODO(721): Write me!
-  return nullptr;
+  scan_clauses = extract_actual_clauses(scan_clauses, false);
+  return make_foreignscan(
+    tlist,
+    scan_clauses,
+    baserel->relid,
+    NIL,
+    NIL,
+    NIL,
+    NIL,
+    outer_plan
+  );
 }
 
 extern "C" void db721_BeginForeignScan(ForeignScanState *node, int eflags) {
